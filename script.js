d2cc1e47dabac0a48fad825b9bb394dcc356481f
lucasbaq <lucasbaquintela@gmail.com>
2022-01-11 17:05:26 -0300

    Termina a 3

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const loginBtn = document.getElementById('trybewarts-login-btn');
+
+loginBtn.addEventListener('click', function (event) {
+  event.preventDefault();
+  const email = document.getElementById('trybewarts-login-email').value;
+  const senha = document.getElementById('trybewarts-login-pwd').value;
+  if (email == "tryber@teste.com" ){
+    if (senha == "123456") {
+      alert("Olá, Tryber!");
+    } else {
+      alert("Email ou senha inválidos.");
+    }
+  } else {
+    alert("Email ou senha inválidos.");
+  }
+});
