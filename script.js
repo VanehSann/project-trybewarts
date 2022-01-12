e4784151569f2ee9944f8e710c9cbea2593691b4
lucasbaq <lucasbaquintela@gmail.com>
2022-01-11 21:26:25 -0300

    Termina o 21

--- script.js
+++ script.js
@@ -26,3 +26,129 @@ agreement.addEventListener('click', function (event) {
     document.getElementById('submit-btn').disabled = true;
   }
 });
+
+function nomeFinal() {
+  const nome = document.getElementById('input-name').value;
+  const sobrenome = document.getElementById('input-lastname').value;
+  const nomeCompleto = nome + ' ' + sobrenome;
+  return nomeCompleto;
+}
+
+function emailFinal() {
+  const email = document.getElementById('input-email').value;
+  return email;
+}
+
+function casaFinal() {
+  const casa = document.getElementById('house').value;
+  return casa;
+}
+
+function familiaFinal() {
+  const familia = document.getElementsByName('family');
+  for (let i = 0; i < familia.length; i += 1) {
+    if (familia[i].checked == true) {
+      let familiaFinal = familia[i].value;
+      return familiaFinal;
+    }
+  }
+}
+
+function materiasFinal() {
+  const materias = document.getElementsByName('conteudo');
+  let materiasFinal = [];
+  for(let i = 0; i < materias.length; i += 1) {
+    if (materias[i].checked == true) {
+      materiasFinal.push(materias[i].value);
+    }
+  }
+  return materiasFinal;
+}
+
+function avaliacaoFinal() {
+  const avaliacao = document.getElementsByClassName('evaluation');
+  for (let i = 0; i < avaliacao.length; i += 1) {
+    if (avaliacao[i].checked == true) {
+      let avaliacaoFinal = avaliacao[i].value;
+      return avaliacaoFinal;
+    }
+  }
+}
+
+function observacaoFinal() {
+  const observacao = document.getElementById('textarea').value;
+  return observacao;
+}
+
+function apagaTudo() {
+  document.getElementById('evaluation-form').remove();
+}
+
+function criaNome(nome) {
+  const p = document.createElement('p');
+  p.innerText = 'Nome: ' + nome;
+  document.getElementById('evaluation-form').appendChild(p);
+}
+
+function criaEmail(email) {
+  const p = document.createElement('p');
+  p.innerText = 'Email: ' + email;
+  document.getElementById('evaluation-form').appendChild(p);
+}
+
+function criaCasa(casa) {
+  const p = document.createElement('p');
+  p.innerText = 'Casa: ' + casa;
+  document.getElementById('evaluation-form').appendChild(p);
+}
+
+function criaFamilia(familia) {
+  const p = document.createElement('p');
+  p.innerText = 'Família: ' + familia;
+  document.getElementById('evaluation-form').appendChild(p);
+}
+
+function criaMaterias(materias) {
+  const p = document.createElement('p');
+  p.innerText = 'Matérias: ' + materias.join(', ');
+  document.getElementById('evaluation-form').appendChild(p);
+}
+
+function criaAvaliacao(avaliacao) {
+  const p = document.createElement('p');
+  p.innerText = 'Avaliação: ' + avaliacao;
+  document.getElementById('evaluation-form').appendChild(p);
+}
+
+function criaObservacao(observacao) {
+  const p = document.createElement('p');
+  p.innerText = 'Observações: ' + observacao;
+  document.getElementById('evaluation-form').appendChild(p);
+}
+
+function criaNovos(nome, email, casa, familia, materias, avaliacao, observacao) {
+  const form = document.createElement('form');
+  form.id = 'evaluation-form';
+  const img = document.getElementById('trybewarts-forms-logo');
+  document.getElementsByTagName('main')[0].insertBefore(form, img);
+  criaNome(nome);
+  criaEmail(email);
+  criaCasa(casa);
+  criaFamilia(familia);
+  criaMaterias(materias);
+  criaAvaliacao(avaliacao);
+  criaObservacao(observacao);
+}
+
+submitBtn.addEventListener('click', function (event) {
+  event.preventDefault();
+  const nome = nomeFinal();
+  const email = emailFinal();
+  const casa = casaFinal();
+  const familia = familiaFinal();
+  const materias = materiasFinal();
+  const avaliacao = avaliacaoFinal();
+  const observacao = observacaoFinal();
+  apagaTudo();
+  criaNovos(nome, email, casa, familia, materias, avaliacao, observacao);
+});
